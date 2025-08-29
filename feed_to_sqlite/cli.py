d6faf127384f0c0057a774d6cea6de0a35798d94
Chris Amico <eyeseast@gmail.com>
2025-08-28 21:32:03 -0400

    Add header option to CLI

--- feed_to_sqlite/cli.py
+++ feed_to_sqlite/cli.py
@@ -11,13 +11,18 @@ from .ingest import ingest_feed
 @click.option(
     "--alter", is_flag=True, default=False, help="Add missing fields to table"
 )
+@click.option(
+    "--header", "-H", nargs=2, multiple=True, help="Add headers to outgoing requests"
+)
 @click.argument(
     "database",
     type=click.Path(exists=False, file_okay=True, dir_okay=False, allow_dash=False),
     required=True,
 )
 @click.argument("urls", nargs=-1)
-def cli(database, urls, table=None, alter=False):
-    with httpx.Client() as client:
+def cli(database, urls, table=None, alter=False, header=None):
+    header = dict(header or {})
+    header.setdefault("user-agent", "feed-to-sqlite")
+    with httpx.Client(headers=header) as client:
         for url in urls:
             ingest_feed(database, table_name=table, url=url, client=client, alter=alter)
